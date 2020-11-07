0b1969a07010772bc8ca7c993ea1bd235eb575fb
hsn <systemry420gmail.com>
2020-11-07 15:44:50 +0200

    add data using a form

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const cafeList = document.querySelector('#cafe-list')
+const form = document.querySelector('#add-cafe-form')
 
 // create elements and render to DOM
 function renderCafe(doc) {
@@ -21,4 +22,18 @@ db.collection('cafe').get().then((snapshot)=>{
     snapshot.docs.forEach(doc => {
         renderCafe(doc);
     });
+})
+
+// listen to form submit
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    // add data using a form
+    db.collection('cafe').add({
+        name: form.name.value,
+        city: form.city.value,
+    })
+
+    form.name.value = ''
+    form.city.value = ''
 })
