0a40459f637a59c92faadb6b7c5c713dc3f117ec
hsn <systemry420gmail.com>
2020-11-07 15:33:53 +0200

    render elements using the retrieved data

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+const cafeList = document.querySelector('#cafe-list')
+
+// create elements and render to DOM
+function renderCafe(doc) {
+    let li = document.createElement('li')
+    let name = document.createElement('span')
+    let city = document.createElement('span')
+
+    li.setAttribute('data-id', doc.id)
+    name.textContent = doc.data().name
+    city.textContent = doc.data().city
+
+    li.appendChild(name)
+    li.appendChild(city)
+    cafeList.appendChild(li)
+}
+
+
+// get data using snapshot
 db.collection('cafe').get().then((snapshot)=>{
     snapshot.docs.forEach(doc => {
-        console.log(doc.data());
+        renderCafe(doc);
     });
 })
