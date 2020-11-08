ad9c66356f2229266c02408e719a032a6b4af470
hsn <systemry420gmail.com>
2020-11-08 11:31:05 +0200

    signIn with email and password

--- auth.js
+++ auth.js
@@ -27,3 +27,22 @@ logout.addEventListener('click', e =>{
         console.log('Logged out');
     })
 })
+
+
+// login
+const loginForm = document.querySelector('#login-form')
+loginForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    const email = loginForm['login-email'].value
+    const password = loginForm['login-password'].value
+
+    auth.signInWithEmailAndPassword(email, password)
+    .then(cred=>{
+        console.log("login", cred.user);
+        const modal = document.querySelector('#modal-login')
+        M.Modal.getInstance(modal).close()
+        loginForm.reset()
+
+    })
+})
