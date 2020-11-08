62e50dd14d8b0d35c69256cac221d0033a3da160
hsn <systemry420gmail.com>
2020-11-08 11:24:30 +0200

    logout user

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
-// signup
-
 const signForm = document.querySelector('#signup-form')
+
+// signup
 signForm.addEventListener('submit', (e)=>{
     e.preventDefault()
 
@@ -17,4 +17,13 @@ signForm.addEventListener('submit', (e)=>{
     })
 
 
+})
+
+// logout
+const logout = document.querySelector('#logout')
+logout.addEventListener('click', e =>{
+    e.preventDefault()
+    auth.signOut().then(()=>{
+        console.log('Logged out');
+    })
 })
